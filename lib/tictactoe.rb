b4edf001ae46475a9e775190b770d261366e0cc8
Jorge Moreno <solisamo6@gmail.com>
2019-09-06 14:31:57 -0500

    Se añadieron las clases en las cuales se va a segmentar el codigo y el inicio del juego para el usuario

---
+++ lib/tictactoe.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require './tictactoe/version'
+require './tictactoe/board'
+require './tictactoe/player'
+require './tictactoe/game'
+require './tictactoe/verify'
+
+player_one = 'X'
+player_two = 'O'
+play_again = ' '
+
